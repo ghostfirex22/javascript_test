be114c75d0935ab088e35b9c13d5f7b860a94e4a
ghostfirex22 <lokmanefire@gmail.com>
2024-01-30 10:19:57 +0100

    first test

---
+++ lesson_1/index.js
@@ -0,0 +1,41 @@
+// les fonction en javascript
+// function test(name){
+//     console.log("hello "+ name)
+// }
+
+// test("mohamed")
+
+// function somme(number1, number2){
+//     return number1 + number2
+// }
+
+// console.log (somme(13,80))
+
+// dexlaration  des variables
+
+// function inscrire(){
+//     let nom;
+//     let prenome;
+//     let ago;
+//     nom = "loukhmi"
+//     prenome = "lokmane"
+//     age = 19
+//     if (age < 22){
+//         var statut;
+//         statut = "stagiaire"
+//     }
+//     console.log(nom)
+//     console.log(statut)
+// }
+
+// let texte1 = "ofppt";
+// texte1 += "";
+// let texte2 = " en force"
+// let texte3 = texte1 + texte2;
+
+// console.log (texte3);
+
+// let x =1 + 1;
+// let y ="5" + 1;
+
+// console.log(x);
---
+++ lesson_2/index2.js
@@ -0,0 +1,18 @@
+let a = 5
+let b = true
+
+if (a == 5 && b == true) {
+    console.log("and")
+}
+
+if (a == 5 || b == true) {
+    console.log("or")
+}
+
+if (b == !false) {
+    console.log("not")
+}
+
+if (b==1 ){
+    console.log("done")
+}
